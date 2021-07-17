ce2b9412ff44228e577456e526003279e22449d2
OnyNwosu <onuwa.nwosu@gmail.com>
2021-07-16 19:02:03 -0500

    adding coordinates

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
 // Function to determine marker size based on population
-function markerSize(population) {
-    return population / 40;
+function markerSize(depth) {
+    return depth * 15;
 }
 
 // An array containing all of the information needed to create city and state markers
@@ -62,83 +62,80 @@ function init() {
             }
         }
     ];
+    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function (earthquakeData) {
+        console.log(earthquakeData);
 
-    // Define arrays to hold created city and state markers
-    var cityMarkers = [];
-    var stateMarkers = [];
+        // Define arrays to hold created city and state markers
+        var index = 0
+        var earthquakeMarkers = [];
+        console.log([earthquakeData.features[index].geometry.coordinates[0],earthquakeData.features[index].geometry.coordinates[1]])
+        // Loop through locations and create city and state markers
+        for (var i = 0; i < earthquakeData.features.length; i++) {
 
-    // Loop through locations and create city and state markers
-    for (var i = 0; i < locations.length; i++) {
-        // Setting the marker radius for the state by passing population into the markerSize function
-        stateMarkers.push(
-            L.circle(locations[i].coordinates, {
-                stroke: false,
-                fillOpacity: 0.75,
-                color: "white",
-                fillColor: "white",
-                radius: markerSize(locations[i].state.population)
-            })
-        );
 
-        // Setting the marker radius for the city by passing population into the markerSize function
-        cityMarkers.push(
-            L.circle(locations[i].coordinates, {
-                stroke: false,
-                fillOpacity: 0.75,
-                color: "purple",
-                fillColor: "purple",
-                radius: markerSize(locations[i].city.population)
-            })
-        );
-    }
+            // Setting the marker radius for the city by passing population into the markerSize function
+            earthquakeMarkers.push(
+                L.circle([earthquakeData.features[i].geometry.coordinates[1],earthquakeData.features[i].geometry.coordinates[0]], {
+                    stroke: false,
+                    fillOpacity: 0.75,
+                    color: "pink",
+                    fillColor: "pink",
+                    // radius: markerSize(earthquakeData.features[i].properties.mag)
+                    radius: 50
+                })
+            );
+        }
 
-    // Create base layers
+        // Create base layers
 
-    // Streetmap Layer
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
-        maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/streets-v11",
-        accessToken: API_KEY
-    });
+        // Streetmap Layer
+        var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+            tileSize: 512,
+            maxZoom: 18,
+            zoomOffset: -1,
+            id: "mapbox/streets-v11",
+            accessToken: API_KEY
+        });
 
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "dark-v10",
-        accessToken: API_KEY
-    });
+        var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            maxZoom: 18,
+            id: "dark-v10",
+            accessToken: API_KEY
+        });
 
-    // Create two separate layer groups: one for cities and one for states
-    var states = L.layerGroup(stateMarkers);
-    var cities = L.layerGroup(cityMarkers);
+        // Create two separate layer groups: one for cities and one for states
+        var cities = L.layerGroup(earthquakeMarkers);
 
-    // Create a baseMaps object
-    var baseMaps = {
-        "Street Map": streetmap,
-        "Dark Map": darkmap
-    };
+        // Create a baseMaps object
+        var baseMaps = {
+            "Dark Map": darkmap,
+            "Street Map": streetmap,
+            
+        };
 
-    // Create an overlay object
-    var overlayMaps = {
-        "State Population": states,
-        "City Population": cities
-    };
+        // Create an overlay object
+        var overlayMaps = {
+            "City Population": cities
+        };
 
-    // Define a map object
-    var myMap = L.map("mapid", {
-        center: [37.09, -95.71],
-        zoom: 5,
-        layers: [streetmap, states, cities]
-    });
+        // Define a map object
+        var myMap = L.map("mapid", {
+            center: [37.09, -95.71],
+            zoom: 5,
+            layers: [darkmap, cities]
+        });
 
-    // Pass our map layers into our layer control
-    // Add the layer control to the map
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
+        // Pass our map layers into our layer control
+        // Add the layer control to the map
+        L.control.layers(baseMaps, overlayMaps, {
+            collapsed: false
+        }).addTo(myMap);
+    })
+
+
+
 };
 
 window.addEventListener('DOMContentLoaded', init);
